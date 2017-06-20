78c398d12235145addd02b4aa2bc30f027d33b26
DmitriyChumak <dimitriy.chumak@gmail.com>
2017-06-20 19:23:10 +0300

    feat

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dima
+ * Date: 16.06.17
+ * Time: 20:57
+ */
+echo '<h1>privet</h1>';
+
+$a=array('potato','pop','smthg <br>');
+var_dump($a);
+echo '<br>';
+print_r($a);
